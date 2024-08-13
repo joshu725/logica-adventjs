259f863dd3be708abeaeaf420a2a104356b33a46
Joshua AR <joshu725@gmail.com>
2024-08-12 19:08:59 -0600

    Mejora al ejercicio 3

--- 3/main.js
+++ 3/main.js
@@ -1,27 +1,26 @@
 function findNaughtyStep(original, modified) {
     let aux = "";
+    let stepLength = 0;
 
     if (original.length >= modified.length) {
         aux = original;
-        for (let i = 0; i < original.length; i++) {
-            if (original[i] !== modified[i]) {
-                return aux[i];
-            }
-        }
+        stepLength = original.length;
     } else {
         aux = modified;
-        for (let i = 0; i < modified.length; i++) {
-            if (original[i] !== modified[i]) {
-                return aux[i];
-            }
+        stepLength = modified.length;
+    }
+
+    for (let i = 0; i < stepLength; i++) {
+        if (original[i] !== modified[i]) {
+            return aux[i];
         }
     }
 
     return "";
 }
 
-console.log(findNaughtyStep('abcd', 'abcde')); // e
-console.log(findNaughtyStep('abcde', 'abcd')); // e
-console.log(findNaughtyStep('xxxx', 'xxoxx')); // o
-console.log(findNaughtyStep('stepfor', 'stepor')); // f
-console.log(findNaughtyStep('iiiii', 'iiiii')); // ""
+console.log(findNaughtyStep("abcd", "abcde")); // e
+console.log(findNaughtyStep("abcde", "abcd")); // e
+console.log(findNaughtyStep("xxxx", "xxoxx")); // o
+console.log(findNaughtyStep("stepfor", "stepor")); // f
+console.log(findNaughtyStep("iiiii", "iiiii")); // ""
